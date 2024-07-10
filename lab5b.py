2d2d5c5e980e05ca64a2510e4fd5529044631d0d
Carole Desiree Cachero <cdcachero@myseneca.ca>
2024-07-10 11:39:05 -0400

    results ok

--- lab5b.py
+++ lab5b.py
@@ -2,6 +2,31 @@
 
 #AuthorID: cdcachero
 
+def read_file_string(file_name):
+    # Takes file_name as string for a file name, returns its entire contents as a string
+    hello_file = open(file_name,'r')
+
+    read_hello = hello_file.read()
+    hello_file.close()
+    return read_hello
+
+def read_file_list(file_name):
+    # Takes a file_name as string for a file name, 
+    # return its entire contents as a list of lines without new-line characters
+    
+    hello_file = open(file_name,'r')
+
+    list_hello = list(hello_file)
+    
+    clean_list = []
+    for line in list_hello:
+        clean_list.append(line.strip('\n'))
+
+    hello_file.close()
+
+    return clean_list
+
+
 def append_file_string(file_name, string_of_lines):
     # Takes two strings, appends the string to the end of the file
     f = open(file_name,'a')
@@ -27,14 +52,6 @@ def copy_file_add_line_numbers(file_name_read, file_name_write):
         ctr+=1
         write_f.write(str(ctr) + ':' + line)
 
-def read_file_string(file_name):
-    #Takes a string as filename and return its content as string
-
-    f = open(file_name,'r')
-    read_file = f.read()
-    f.close()
-    return read_file
-
 if __name__ == '__main__':
     file1 = 'seneca1.txt'
     file2 = 'seneca2.txt'
@@ -47,3 +64,4 @@ if __name__ == '__main__':
     print(read_file_string(file2))
     copy_file_add_line_numbers(file2, file3)
     print(read_file_string(file3))
+                                               
